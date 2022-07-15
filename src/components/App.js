6250b55820b25b13bf8a6f8e3a1b5327a91ebd0b
Rahul <rk255009@gmail.com>
2022-07-15 16:26:42 +0530

    added export

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main"> <p id="root"></p>I am learning React. My life is getting better.</div>
   )
 }
 
